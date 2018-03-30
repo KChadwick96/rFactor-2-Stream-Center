639916a13175eb7eb8f263adaa7dcf6eac692daf
Kieran Chadwick <kieran.chadwick@outlook.com>
2018-03-30 08:35:17 +0100

    styling updates

--- app/index.js
+++ app/index.js
@@ -1,76 +1,57 @@
 const Mustache = require('mustache');
 
-const CAMERAS = [
-    {
-        id: 'scv_nosecam',
-        name: 'Nosecam',
-        class: 'primary'
-    },
-    {
-        id: 'scv_swingman',
-        name: 'Swingman',
-        class: 'secondary'
-    },
-    {
-        id: 'scv_onboard001',
-        name: 'TCAM Left',
-        class: 'success'
-    },
-    {
-        id: 'scv_onboard002',
-        name: 'TCAM Rear',
-        class: 'danger'
-    },
-    {
-        id: 'scv_onboard003',
-        name: 'Left Airbox',
-        class: 'warning'
-    },
-    {
-        id: 'scv_onboard004',
-        name: 'Helicopter',
-        class: 'info'
-    }
-];
+const CAMERAS = [{
+    id: 'scv_nosecam',
+    name: 'Nosecam',
+    class: 'primary'
+},{
+    id: 'scv_swingman',
+    name: 'Swingman',
+    class: 'secondary'
+},{
+    id: 'scv_onboard001',
+    name: 'TCAM Left',
+    class: 'success'
+},{
+    id: 'scv_onboard002',
+    name: 'TCAM Rear',
+    class: 'danger'
+},{
+    id: 'scv_onboard003',
+    name: 'Left Airbox',
+    class: 'warning'
+},{
+    id: 'scv_onboard004',
+    name: 'Helicopter',
+    class: 'info'
+}];
 
 const drivers = [
-    {name: 'Kieran Chadwick', live: false},
-    {name: 'Craig Baxter', live: true},
-    {name: 'Sam Carpenter', live: false},
-    {name: 'Scott Davison', live: false}
+    {name: 'Kieran Chadwick', live: false, position: 1},
+    {name: 'Craig Baxter', live: true, position: 2},
+    {name: 'Sam Carpenter', live: false, position: 3},
+    {name: 'Scott Davison', live: false, position: 4}
 ];
 
 renderDrivers();
 
 function renderDrivers() {
-    const cameraButtons = getCameraButtons();
-
     const template = $('#driver-template').html();
+    const cameraPartial = $('#camera-button-template').html();
 
     let driversHtml = '';
     drivers.forEach((driver, index) => {
-        const selected = (driver.live) ? 'driver--selected' : '';
-        driversHtml += Mustache.to_html(template, {
+        const data = {
             name: driver.name,
-            selected,
-            buttons: cameraButtons
-        });
+            position: driver.position,
+            selected: (driver.live) ? 'driver--selected' : '',
+            cameras: CAMERAS
+        }
+        const partials = {camera: cameraPartial};
+        console.log(data);
+        console.log(partials);
+        driversHtml += Mustache.to_html(template, data, partials);
     });
 
     $('#drivers').html(driversHtml);
-}
-
-function getCameraButtons() {
-    const template = $('#camera-button-template').html();
-
-    let buttons = '';
-    CAMERAS.forEach(camera => {
-        Mustache.render
-        buttons += Mustache.render(template, {
-            name: camera.name,
-            class: camera.class
-        });
-    });
-
-    return buttons;
 }
